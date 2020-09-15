d5f74b7aad57c9cd46e470c39ecd86e577b0f228
Ruben <ruben.cns@gmail.com>
2020-09-15 18:33:19 +0200

    Add useOnClickOutside hook for Search component.

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import handleRoute from '../../utils/handle-route';
+import useOnClickOutside from '../../hooks/use-onclick-outside';
 import { ReactComponent as SearchIcon } from '../../assets/svg/search.svg';
 import { ReactComponent as AngleDownIcon } from '../../assets/svg/angle-down.svg';
 import { ReactComponent as AngleUpIcon } from '../../assets/svg/angle-up.svg';
@@ -8,9 +9,11 @@ import SearchStyle from './search-style';
 
 export default ({ input, setInput, region, setRegion, countries, setCountries }) => {
   const [results, setResults] = useState([]);
-  const [isHidden, setHidden] = useState({search: true, filter: true});
+  const [isHidden, setHidden] = useState({ search: true, filter: true });
   const regions = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   const history = useHistory();
+  const ref = useRef();
+  useOnClickOutside(ref, () => setHidden({search: true, filter: true}));
 
   const handleInput = ({ target : { value } }) => {
     setInput(value.toLowerCase());
@@ -38,7 +41,7 @@ export default ({ input, setInput, region, setRegion, countries, setCountries })
       <form onSubmit={handleForm} className="search-input element">
         <button type="submit"><SearchIcon className="icon" /></button>
         <input type="text" placeholder="Search for a country..." value={input} onChange={handleInput} />
-        <div className="dropdown element">
+        <div className="dropdown element" ref={ref}>
           {results && 
             results.map(r => 
               <span className="dropdown-item" onClick={() => handleRoute(history, r.alpha3Code)} key={r.alpha3Code}>{r.name}</span>
@@ -49,7 +52,7 @@ export default ({ input, setInput, region, setRegion, countries, setCountries })
         onClick={() => setHidden({...isHidden, filter: !isHidden.filter})}>
         <span>{region === 'All' ? 'Filter by Region' : region}</span>
         {isHidden ? <AngleUpIcon className="icon" /> : <AngleDownIcon className="icon" />}
-        <div className="dropdown element">
+        <div className="dropdown element" ref={ref}>
           {regions.map(e => 
             <span className="dropdown-item" onClick={() => setRegion(e)} key={e}>{e}</span>
           )}
